7171b447946919ee1f6db9031bdbdb5a47519156
Терещук Дарья <,     GitHub>
2025-09-25 18:34:35 +0300

    add: first function

---
+++ function.js
@@ -0,0 +1,5 @@
+const multiply = (firstNamber,secondNamber)=>{
+    return firstNamber*secondNamber
+}
+
+console.log(multiply(845686,55735))
---
+++ var.js
@@ -0,0 +1,6 @@
+const greeting = "привет мир"
+console.log(greeting)
+const firstNumber = 5
+const secondNumber = 33
+const result = firstNumber*secondNumber
+console.log(result)
